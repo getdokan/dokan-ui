8501b893e36850d17640ea09edf0599a982d8ad8
Tanmay Mishu <tanmaymishu@gmail.com>
2021-12-02 18:40:37 +0600

    Disable lint on github action

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Divider } from '../stories/Divider.stories';
 
-describe('Thing', () => {
+describe('Divider', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Divider label="Or continue with" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
