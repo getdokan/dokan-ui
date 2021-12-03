1cca8725ed455f2b567dd38e2951370e80254095
Tanmay Mishu <tanmaymishu@gmail.com>
2021-12-03 18:49:30 +0600

    Allow error message in SimpleSelect (#4)

--- stories/SimpleSelect.stories.tsx
+++ stories/SimpleSelect.stories.tsx
@@ -12,8 +12,21 @@ export default meta;
 const Template: Story<SimpleSelectProps> = args => <SimpleSelect {...args} />;
 
 export const Default = Template.bind({});
+export const WithError = Template.bind({});
 
 Default.args = {
+    label: 'Fruits',
+    labelKey: 'name',
+    valueKey: 'id',
+    options: [
+        { id: 1, name: 'Apple' },
+        { id: 2, name: 'Orange' },
+        { id: 3, name: 'Banana' },
+    ]
+};
+
+WithError.args = {
+    error: "The fruits field is required",
     label: 'Fruits',
     labelKey: 'name',
     valueKey: 'id',
