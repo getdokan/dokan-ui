ed9347fab481d0fdcbfb189b532a9708a02fb840
Md Mostafizur Rahman <42089253+MdMostaFizurRahaman@users.noreply.github.com>
2021-12-13 13:46:29 +0600

    Add button responsive icon (#9)

--- src/Button.tsx
+++ src/Button.tsx
@@ -5,8 +5,9 @@ export interface ButtonProps {
     disabled?: boolean,
     icon?: any,
     color: string,
-    label: string,
+    label?: string,
     block?: boolean,
+    iconOnMobile?: boolean,
     onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
@@ -24,7 +25,7 @@ const Button: FC<ButtonProps> = (props) => {
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>}
                 {props.icon && <Icon className="w-5 h-5 mr-1" />}
-                {props.label}
+                {props.label && <span className={`${props.iconOnMobile ? "sm:hidden" : ""}`}>{props.label}</span>}
                 {props.children}
             </button>
         </>
