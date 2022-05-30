8827281c6a47334141aca79d5db62d5db0b202fe
Tanmay Mishu <tanmaymishu@gmail.com>
2022-05-30 10:03:03 +0600

    Polish combobox

--- src/ComboBox.tsx
+++ src/ComboBox.tsx
@@ -14,7 +14,7 @@
   }
   ```
 */
-import React, { Key, useState } from 'react';
+import React, { Key, useEffect, useRef, useState } from 'react';
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 import { Combobox } from '@headlessui/react';
 
@@ -38,6 +38,13 @@ const ComboBox = <TItem,>(props: ComboBoxProps<TItem>) => {
   const [query, setQuery] = useState('');
   const [selectedItems, setSelectedItems] = useState([]);
   const [inputHasFocus, setInputHasFocus] = useState(false);
+  const inputField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputHasFocus) {
+      inputField.current && inputField.current.focus();
+    }
+  }, [inputHasFocus]);
 
   const filteredItems =
     query === ''
@@ -54,30 +61,36 @@ const ComboBox = <TItem,>(props: ComboBoxProps<TItem>) => {
     <Combobox value={selectedItems} onChange={setSelectedItems} multiple={props.multiple}>
       <Combobox.Label className="block text-sm font-medium text-gray-700">{props.label || 'Search and Select'}</Combobox.Label>
       <div className="relative mt-1">
-        <section className="absolute top-1.5 left-1.5">
-          {(props.multiple && !inputHasFocus) && selectedItems.map(selectedItem => {
-            return <span className="ml-1 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-indigo-100 text-primary-800">
-            {selectedItem[props.valueFrom]}
-            <button
-              type="button"
-              className="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-white"
-            >
-              <span className="sr-only">Remove {selectedItem[props.valueFrom]} option</span>
-              <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8" onClick={() => removeItem(selectedItem)}>
-                <path strokeLinecap="round" strokeWidth="1.5" d="M1 1l6 6m0-6L1 7" />
-              </svg>
-            </button>
-          </span>
-          })}
-        </section>
-        <Combobox.Input
+        {(inputHasFocus && selectedItems.length > 0) && <span onClick={() => setInputHasFocus(false)} className="hover:cursor-pointer bg-primary-600 text-white rounded-3xl px-1.5 py-0.5 text-xs absolute top-2.5 right-8">+{selectedItems.length}</span>}
+        {(selectedItems.length < 1 || inputHasFocus) && <Combobox.Input
+          ref={inputField}
           onFocus={() => setInputHasFocus(true)}
           onBlur={() => setInputHasFocus(false)}
           placeholder={selectedItems.length < 1 || inputHasFocus ? "Search..." : ""}
           className={`w-full rounded border border-gray-300 bg-white pl-3 pr-10 focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm ${props.inputClasses}`}
           onChange={(event) => setQuery(event.target.value)}
           displayValue={(item: any) => item[props.valueFrom]}
-        />
+        />}
+        {(!inputHasFocus && selectedItems.length > 0) && 
+        <section
+          onClick={() => setInputHasFocus(true)}
+          className="hover:cursor-text mt-1 p-2 w-full rounded border border-gray-300 bg-white pl-3 pr-10 focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm">
+          {(props.multiple && !inputHasFocus) && selectedItems.map(selectedItem => {
+            return <span key={selectedItem[props.keyFrom]} className="mr-1 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-indigo-100 text-primary-800">
+            {selectedItem[props.valueFrom]}
+            <button
+              type="button"
+              onClick={(e) => {e.stopPropagation(); removeItem(selectedItem)}}
+              className="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-white"
+            >
+              <span className="sr-only">Remove {selectedItem[props.valueFrom]} option</span>
+              <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path strokeLinecap="round" strokeWidth="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+          })}
+        </section>}
         <Combobox.Button className={`absolute inset-y-0 right-0 flex items-center rounded-r px-2 focus:outline-none ${props.buttonClasses}`}>
           <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
         </Combobox.Button>
@@ -90,7 +103,7 @@ const ComboBox = <TItem,>(props: ComboBoxProps<TItem>) => {
                 value={item}
                 className={({ active }) =>
                   classNames(
-                    'relative cursor-default select-none py-2 pl-3 pr-9',
+                    'relative cursor-pointer select-none py-2 pl-3 pr-9',
                     active ? 'bg-primary-600 text-white' : 'text-gray-900',
                     props.optionClasses as string
                   )
