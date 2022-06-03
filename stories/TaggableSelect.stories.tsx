93ff7af0e4003cf2e8fe28272741b873bdee474d
MdMostaFizurRahaman <asgraphicsolution@gmail.com>
2022-06-03 10:42:54 +0600

    Add Searchable & Taggable Select

---
+++ stories/TaggableSelect.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import SearchableSelect from '../src/SearchableSelect';
+
+const meta: Meta = {
+  title: 'SearchableSelect',
+  component: SearchableSelect,
+};
+
+export default meta;
+
+const Template: Story = (args) => <SearchableSelect {...args} />;
+
+export const Default = Template.bind({});
+export const Multiple = Template.bind({});
+
+Default.args = {
+  className: 'mt-2',
+  options: [
+    { label: 'Bangladesh', value: 'BD' },
+    { label: 'India', value: 'IN' },
+    { label: 'Pakistan', value: 'PK' },
+    { label: 'Sri Lanka', value: 'SL' },
+  ],
+  label: 'Countries',
+  onChange: (item) => {
+    console.log(item);
+  },
+};
+
+Multiple.args = {
+  className: 'mt-2',
+  options: [
+    { label: 'Bangladesh', value: 'BD' },
+    { label: 'India', value: 'IN' },
+    { label: 'Pakistan', value: 'PK' },
+    { label: 'Sri Lanka', value: 'SL' },
+  ],
+  label: 'Countries',
+  isMulti: true,
+  onChange: (items) => {
+    console.log(items);
+  },
+  helpText: '',
+};
