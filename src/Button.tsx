c57579c5e68c3c48cbf4739914b0d63b23657c9f
MdMostaFizurRahaman <asgraphicsolution@gmail.com>
2022-06-03 11:24:37 +0600

    Fix outline button text color issue

--- src/Button.tsx
+++ src/Button.tsx
@@ -27,7 +27,7 @@ export interface ButtonProps {
     | 'primary'
     | 'secondary'
     | 'danger'
-    | 'white'
+    | 'white';
   label?: string;
   type?: 'button' | 'submit' | 'reset';
   block?: boolean;
@@ -39,27 +39,111 @@ const Button: FC<ButtonProps> = (props) => {
   const Icon = props.icon;
 
   const colors = {
-    amber: `${props.outlined ? "border border-amber-500 text-amber-500 hover:text-white" : "bg-amber-600"} hover:bg-amber-700 focus:ring-amber-500`,
-    blue: `${props.outlined ? "border border-blue-500 text-blue-500 hover:text-white" : "bg-blue-600"} hover:bg-blue-700 focus:ring-blue-500`,
-    cyan: `${props.outlined ? "border border-cyan-500 text-cyan-500 hover:text-white" : "bg-cyan-600"} hover:bg-cyan-700 focus:ring-cyan-500`,
-    emerald: `${props.outlined ? "border border-emerald-500 text-emerald-500 hover:text-white" : "bg-emerald-600"} hover:bg-emerald-700 focus:ring-emerald-500`,
-    fuchsia: `${props.outlined ? "border border-fuchsia-500 text-fuchsia-500 hover:text-white" : "bg-fuchsia-600"} hover:bg-fuchsia-700 focus:ring-fuchsia-500`,
-    green: `${props.outlined ? "border border-green-500 text-green-500 hover:text-white" : "bg-green-600"} hover:bg-green-700 focus:ring-green-500`,
-    gray: `${props.outlined ? "border border-gray-500 text-gray-500 hover:text-white" : "bg-gray-600"} hover:bg-gray-700 focus:ring-gray-500`,
-    indigo: `${props.outlined ? "border border-indigo-500 text-indigo-500 hover:text-white" : "bg-indigo-600"} hover:bg-indigo-700 focus:ring-indigo-500`,
-    lime: `${props.outlined ? "border border-lime-500 text-lime-500 hover:text-white" : "bg-lime-600"} hover:bg-lime-700 focus:ring-lime-500`,
-    orange: `${props.outlined ? "border border-orange-500 text-orange-500 hover:text-white" : "bg-orange-600"} hover:bg-orange-700 focus:ring-orange-500`,
-    pink: `${props.outlined ? "border border-pink-500 text-pink-500 hover:text-white" : "bg-pink-600"} hover:bg-pink-700 focus:ring-pink-500`,
-    purple: `${props.outlined ? "border border-purple-500 text-purple-500 hover:text-white" : "bg-purple-600"} hover:bg-purple-700 focus:ring-purple-500`,
-    red: `${props.outlined ? "border border-red-500 text-red-500 hover:text-white" : "bg-red-600"} hover:bg-red-700 focus:ring-red-500`,
-    rose: `${props.outlined ? "border border-rose-500 text-rose-500 hover:text-white" : "bg-rose-600"} hover:bg-rose-700 focus:ring-rose-500`,
-    sky: `${props.outlined ? "border border-sky-500 text-sky-500 hover:text-white" : "bg-sky-600"} hover:bg-sky-700 focus:ring-sky-500`,
-    teal: `${props.outlined ? "border border-teal-500 text-teal-500 hover:text-white" : "bg-teal-600"} hover:bg-teal-700 focus:ring-teal-500`,
-    violet: `${props.outlined ? "border border-violet-500 text-violet-500 hover:text-white" : "bg-violet-600"} hover:bg-violet-700 focus:ring-violet-500`,
-    yellow: `${props.outlined ? "border border-yellow-500 text-yellow-500 hover:text-white" : "bg-yellow-600"} hover:bg-yellow-700 focus:ring-yellow-500`,
-    primary: `${props.outlined ? "border border-primary-500 text-primary-500 hover:text-white" : "bg-primary-600"} hover:bg-primary-700 focus:ring-primary-500`,
-    secondary: `${props.outlined ? "border border-secondary-500 text-secondary-500 hover:text-white" : "bg-secondary-600"} hover:bg-secondary-700 focus:ring-secondary-500`,
-    danger: `${props.outlined ? "border border-danger-500 text-danger-500 hover:text-white" : "bg-danger-600"} hover:bg-danger-700 focus:ring-danger-500`,
+    amber: `${
+      props.outlined
+        ? 'border border-amber-500 text-amber-500 hover:text-white'
+        : 'bg-amber-600 text-white'
+    } hover:bg-amber-700 focus:ring-amber-500`,
+    blue: `${
+      props.outlined
+        ? 'border border-blue-500 text-blue-500 hover:text-white'
+        : 'bg-blue-600 text-white'
+    } hover:bg-blue-700 focus:ring-blue-500`,
+    cyan: `${
+      props.outlined
+        ? 'border border-cyan-500 text-cyan-500 hover:text-white'
+        : 'bg-cyan-600 text-white'
+    } hover:bg-cyan-700 focus:ring-cyan-500`,
+    emerald: `${
+      props.outlined
+        ? 'border border-emerald-500 text-emerald-500 hover:text-white'
+        : 'bg-emerald-600 text-white'
+    } hover:bg-emerald-700 focus:ring-emerald-500`,
+    fuchsia: `${
+      props.outlined
+        ? 'border border-fuchsia-500 text-fuchsia-500 hover:text-white'
+        : 'bg-fuchsia-600 text-white'
+    } hover:bg-fuchsia-700 focus:ring-fuchsia-500`,
+    green: `${
+      props.outlined
+        ? 'border border-green-500 text-green-500 hover:text-white'
+        : 'bg-green-600 text-white'
+    } hover:bg-green-700 focus:ring-green-500`,
+    gray: `${
+      props.outlined
+        ? 'border border-gray-500 text-gray-500 hover:text-white'
+        : 'bg-gray-600 text-white'
+    } hover:bg-gray-700 focus:ring-gray-500`,
+    indigo: `${
+      props.outlined
+        ? 'border border-indigo-500 text-indigo-500 hover:text-white'
+        : 'bg-indigo-600 text-white'
+    } hover:bg-indigo-700 focus:ring-indigo-500`,
+    lime: `${
+      props.outlined
+        ? 'border border-lime-500 text-lime-500 hover:text-white'
+        : 'bg-lime-600 text-white'
+    } hover:bg-lime-700 focus:ring-lime-500`,
+    orange: `${
+      props.outlined
+        ? 'border border-orange-500 text-orange-500 hover:text-white'
+        : 'bg-orange-600 text-white'
+    } hover:bg-orange-700 focus:ring-orange-500`,
+    pink: `${
+      props.outlined
+        ? 'border border-pink-500 text-pink-500 hover:text-white'
+        : 'bg-pink-600 text-white'
+    } hover:bg-pink-700 focus:ring-pink-500`,
+    purple: `${
+      props.outlined
+        ? 'border border-purple-500 text-purple-500 hover:text-white'
+        : 'bg-purple-600 text-white'
+    } hover:bg-purple-700 focus:ring-purple-500`,
+    red: `${
+      props.outlined
+        ? 'border border-red-500 text-red-500 hover:text-white'
+        : 'bg-red-600 text-white'
+    } hover:bg-red-700 focus:ring-red-500`,
+    rose: `${
+      props.outlined
+        ? 'border border-rose-500 text-rose-500 hover:text-white'
+        : 'bg-rose-600 text-white'
+    } hover:bg-rose-700 focus:ring-rose-500`,
+    sky: `${
+      props.outlined
+        ? 'border border-sky-500 text-sky-500 hover:text-white'
+        : 'bg-sky-600 text-white'
+    } hover:bg-sky-700 focus:ring-sky-500`,
+    teal: `${
+      props.outlined
+        ? 'border border-teal-500 text-teal-500 hover:text-white'
+        : 'bg-teal-600 text-white'
+    } hover:bg-teal-700 focus:ring-teal-500`,
+    violet: `${
+      props.outlined
+        ? 'border border-violet-500 text-violet-500 hover:text-white'
+        : 'bg-violet-600 text-white'
+    } hover:bg-violet-700 focus:ring-violet-500`,
+    yellow: `${
+      props.outlined
+        ? 'border border-yellow-500 text-yellow-500 hover:text-white'
+        : 'bg-yellow-600 text-white'
+    } hover:bg-yellow-700 focus:ring-yellow-500`,
+    primary: `${
+      props.outlined
+        ? 'border border-primary-500 text-primary-500 hover:text-white'
+        : 'bg-primary-600 text-white'
+    } hover:bg-primary-700 focus:ring-primary-500`,
+    secondary: `${
+      props.outlined
+        ? 'border border-secondary-500 text-secondary-500 hover:text-white'
+        : 'bg-secondary-600 text-white'
+    } hover:bg-secondary-700 focus:ring-secondary-500`,
+    danger: `${
+      props.outlined
+        ? 'border border-danger-500 text-danger-500 hover:text-white'
+        : 'bg-danger-600 text-white'
+    } hover:bg-danger-700 focus:ring-danger-500`,
     white: `bg-transparent text-gray-600 hover:bg-gray-100`,
     // transparent: `border border-primary-500 text-primary-500 select-none hover:text-white hover:bg-primary-600 focus:shadow-outline`,
   };
@@ -72,9 +156,11 @@ const Button: FC<ButtonProps> = (props) => {
         onClick={props.onClick}
         className={`${props.block ? 'w-full' : ''} ${
           props.disabled ? 'opacity-50 cursor-not-allowed' : ''
-        } flex justify-center py-2 px-4 border rounded shadow-sm text-sm font-medium text-white ${
+        } flex justify-center py-2 px-4 border rounded shadow-sm text-sm font-medium ${
           colors[props.color]
-        } focus:outline-none focus:ring-2 focus:ring-offset-2 ${props.className || ''}`}
+        } focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+          props.className || ''
+        }`}
       >
         {props.loading && (
           <svg
@@ -83,7 +169,14 @@ const Button: FC<ButtonProps> = (props) => {
             fill="none"
             viewBox="0 0 24 24"
           >
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
             <path
               className="opacity-75"
               fill="currentColor"
@@ -92,7 +185,13 @@ const Button: FC<ButtonProps> = (props) => {
           </svg>
         )}
         {props.icon && <Icon className="w-5 h-5 mr-2" />}
-        {props.label && <span className={`${props.iconOnMobile ? 'hidden sm:inline-block' : ''}`}>{props.label}</span>}
+        {props.label && (
+          <span
+            className={`${props.iconOnMobile ? 'hidden sm:inline-block' : ''}`}
+          >
+            {props.label}
+          </span>
+        )}
         {props.children}
       </button>
     </>
