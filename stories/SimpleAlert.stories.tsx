e406e6b81377c3ed7ec424b1c021c4d9a28e8aad
Arafat Islam <arafatkn@gmail.com>
2022-08-30 14:34:44 +0600

    Added description option with Simple Alert component

--- stories/SimpleAlert.stories.tsx
+++ stories/SimpleAlert.stories.tsx
@@ -10,11 +10,24 @@ const meta: Meta = {
 export default meta;
 
 const Template: Story<SimpleAlertProps> = args => <SimpleAlert {...args} />;
+const TemplateWithDescription: Story<SimpleAlertProps> = args => <SimpleAlert {...args}>
+    <div className="text-sm mt-1">
+        We are not able to process your request now.<br/>
+        You can learn more about this.
+    </div>
+</SimpleAlert>;
 
 export const Default = Template.bind({});
+export const WithDescription = TemplateWithDescription.bind({});
 
 Default.args = {
     label: 'Successfully Created',
     type: "success",
     color: 'green'
+};
+
+WithDescription.args = {
+    label: 'Something is wrong!',
+    type: "info",
+    color: 'yellow',
 };
