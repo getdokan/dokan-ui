ec4ca4a12dbdb41a1c39dc968487091eb35e6c9b
Md Mostafizur Rahman <42089253+mostafizurhimself@users.noreply.github.com>
2022-09-09 16:12:22 +0600

    Feat/resolve dependency (#35)
    * Resove external dependencies
    
    * Bundle tailwindcss classes

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import typescript from 'rollup-plugin-typescript2'
 import alias from '@rollup/plugin-alias';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss'
+
 
 import pkg from './package.json'
 
@@ -23,6 +27,12 @@ export default {
                 { find: '@', replacement: '.' },
             ]
         }),
+        postcss({
+            config: "./postcss.config.js"
+        }),
+        commonjs(),
+        nodeResolve()
     ],
-    external: ['react', 'react-dom', 'speakingurl', 'classnames', '@heroicons/react', '@heroicons/react/solid', '@heroicons/react/outline', '@headlessui/react', 'react-laag', 'react-select', 'react-select/creatable', 'react-feather']
+    // external: ['react', 'react-dom', 'speakingurl', 'classnames', '@heroicons/react', '@heroicons/react/solid', '@heroicons/react/outline', '@headlessui/react', 'react-laag', 'react-select', 'react-select/creatable', 'react-feather']
+    external: ['react', 'react-dom']
 }
