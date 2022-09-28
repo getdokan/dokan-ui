3200d6f7569c7ad06bc5660fd689ae483c492f95
Tanmay Mishu <tanmaymishu@gmail.com>
2022-09-28 11:15:50 +0600

    Update story for box

--- stories/Box.stories.tsx
+++ stories/Box.stories.tsx
@@ -3,16 +3,24 @@ import { Meta, Story } from '@storybook/react';
 import Box, { BoxProps } from '../src/Box';
 
 const meta: Meta = {
-    title: 'Box',
-    component: Box,
+  title: 'Box',
+  component: Box,
 };
 
 export default meta;
 
-const Template: Story<BoxProps> = args => <Box {...args}>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad nobis dolorem cupiditate pariatur velit accusamus qui id asperiores? Odio eos reiciendis esse assumenda porro, animi quidem molestias numquam cumque.
-</Box>;
+const Template: Story<BoxProps> = (args) => (
+  <Box {...args}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad nobis
+    dolorem cupiditate pariatur velit accusamus qui id asperiores? Odio eos
+    reiciendis esse assumenda porro, animi quidem molestias numquam cumque.
+  </Box>
+);
 
 export const Default = Template.bind({});
+export const WithShadow = Template.bind({});
 
 Default.args = {};
+WithShadow.args = {
+  shadow: true,
+};
