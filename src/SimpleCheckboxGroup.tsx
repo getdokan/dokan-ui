3ba1e53e618a6ddc770ed4c9071b1427c46c9ab3
Arafat Islam <80309866+arafatkn@users.noreply.github.com>
2022-12-07 12:26:02 +0600

    Simple Checkbox Group (#46)
    * Simple Checkbox Group
    
    * Accept both number and string as value
    
    * Added indeterminate state

---
+++ src/SimpleCheckboxGroup.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import SimpleCheckbox from './SimpleCheckbox';
+
+export interface SimpleCheckboxGroupProps {
+  name?: string;
+  label?: string;
+  helpText?: string;
+  className?: string;
+  options: Array<{
+    value: string | number;
+    label: string;
+    indeterminate?: boolean;
+  }>;
+  defaultValue?: Array<string | number>;
+  errors?: string[];
+  onChange?: (checkedList: Array<string | number>) => void;
+}
+
+const SimpleCheckboxGroup: React.FC<SimpleCheckboxGroupProps> = (props) => {
+  const [checkedList, setCheckedList] = useState<Array<string | number>>(props.defaultValue ?? []);
+
+  useEffect(() => props.onChange && props.onChange(checkedList), [checkedList]);
+
+  return (
+    <>
+      {props.label && (
+        <label className={`block w-full text-sm font-medium text-gray-700 ${!props.helpText ? 'mb-2' : ''}`}>
+          {props.label}
+        </label>
+      )}
+
+      {props.helpText && <p className="text-xs mb-2 leading-5 text-gray-600">{props.helpText}</p>}
+
+      <div className="space-y-0">
+        {props.options.map((option, optionIndex) => (
+          <div key={optionIndex} className={`py-1 ${props.className}`}>
+            <SimpleCheckbox
+              label={option.label}
+              input={{
+                id: `simple-checkbox-group-${option.value}`,
+                type: 'checkbox',
+                name: props.name,
+                value: option.value,
+              }}
+              checked={!!checkedList.find((val) => val === option.value)}
+              indeterminate={option.indeterminate}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setCheckedList((list) => [...list, option.value]);
+                } else {
+                  setCheckedList((list) => list.filter((val) => val !== option.value));
+                }
+              }}
+            />
+          </div>
+        ))}
+      </div>
+
+      {props.errors && (
+        <p className="mt-1.5 text-xs text-red-600">{props.errors.join(', ')}</p>
+      )}
+    </>
+  );
+};
+
+export default SimpleCheckboxGroup;
