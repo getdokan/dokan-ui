4c5138e0bf9634ef5549cba98921af363cc2845e
Arafat Islam <arafatkn@gmail.com>
2022-12-20 21:57:32 +0600

    fixes error detection for empty array
    Boolean of empty array in js is truthy. So, we need to check array length to detect errors properly.

--- src/SimpleInput.tsx
+++ src/SimpleInput.tsx
@@ -42,6 +42,10 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
     }
   }, [props.defaultValue, props.value]);
 
+  const hasErrors = () => {
+    return props.errors && props.errors.length > 0;
+  };
+
   const validClasses = `${
     props.icon && 'pl-10'
   } appearance-none block w-full pl-3 ${
@@ -76,7 +80,7 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <Icon
               className={`h-5 w-5 text-gray-400 ${
-                props.errors && 'text-red-400'
+                hasErrors() && 'text-red-400'
               }`}
               aria-hidden="true"
             />
@@ -89,7 +93,7 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
           id={props.input.id}
           defaultValue={props.defaultValue}
           className={classNames(
-            props.errors?.length ? errorClasses : validClasses,
+            hasErrors() ? errorClasses : validClasses,
             props.className,
             props.disabled && 'disabled'
           )}
@@ -103,13 +107,13 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
           onKeyPress={props.onKeyPress}
           onKeyUp={props.onKeyUp}
           onBlur={props.onBlur}
-          aria-invalid={props.errors ? 'true' : 'false'}
+          aria-invalid={hasErrors() ? 'true' : 'false'}
           aria-describedby={`${props.input.id}-error`}
         />
         {props.counter && (
           <div
             className={`absolute inset-y-0 right-0 flex items-center ${
-              props.errors?.length ? 'pr-8' : 'pr-3'
+              hasErrors() ? 'pr-8' : 'pr-3'
             }`}
           >
             <span className="border-l-2 pl-2 text-gray-400 sm:text-sm">
@@ -124,7 +128,7 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
             </span>
           </>
         )}
-        {props.errors && (
+        {hasErrors() && (
           <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
             <ExclamationCircleIcon
               className="h-5 w-5 text-red-400 sm:h-4 sm:w-4"
@@ -134,9 +138,9 @@ const SimpleInput: React.FC<SimpleInputProps> = (props) => {
         )}
         {props.children}
       </div>
-      {props.errors && (
+      {hasErrors() && (
         <p className="text-xs text-red-600" id={`${props.input.id}-error`}>
-          {props.errors.join(', ')}
+          {props.errors?.join(', ')}
         </p>
       )}
       {props.helpText && (
