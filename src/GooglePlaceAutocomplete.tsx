c45edb34ee8b2edd0c56fc4a73cefd9eadd7a6d8
Hasib <hasib.webdev@gmail.com>
2023-02-25 21:03:27 +0600

    Update autocomplete (#63)
    * installed deps and format lint
    
    * Added Placeautocomplete and Loader component
    
    * type improvement
    
    * removed real api key
    
    * center clear button
    
    * center clear button

--- src/GooglePlaceAutocomplete.tsx
+++ src/GooglePlaceAutocomplete.tsx
@@ -154,7 +154,7 @@ const GooglePlaceAutocomplete: FC<GooglePlaceAutocompleteProps> = ({
           onFocus={onFocus}
           onBlur={onBlur}
         />
-        <div className="absolute top-2 right-2">
+        <div className="absolute right-2 top-1/2 -translate-y-1/2">
           <button
             type="button"
             onClick={() => {
