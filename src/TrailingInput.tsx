5662ce4e0b90980f6e96fa0a2d2726b512fbdfdb
Hasibur Rahman <hasib.webdev@gmail.com>
2023-04-12 09:51:56 +0600

    changed indigo to primary color (#67)
    * changed indigo to primary
    
    * match traling input label with simpleInput

--- src/TrailingInput.tsx
+++ src/TrailingInput.tsx
@@ -14,7 +14,7 @@ export interface TrailingInputProps extends SimpleInputProps {
 const TrailingInput: React.FC<TrailingInputProps> = (props) => {
   const Icon = props.icon;
   const BtnIcon = props.btnIcon;
-  const validClasses = `focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l ${
+  const validClasses = `focus:ring-primary-500 focus:border-primary-500 block w-full rounded-none rounded-l ${
     props.icon && 'pl-10'
   } sm:text-sm border-gray-300 placeholder-gray-400`;
   const errorClasses = `${
@@ -22,7 +22,10 @@ const TrailingInput: React.FC<TrailingInputProps> = (props) => {
   } block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-none rounded-l`;
   return (
     <>
-      <label htmlFor={props.input.id} className="block text-sm font-medium">
+      <label
+        htmlFor={props.input.id}
+        className="block text-sm font-medium text-gray-700 mb-1"
+      >
         {props.label}
       </label>
       <div className={`flex rounded shadow-sm ${props.className}`}>
@@ -57,7 +60,7 @@ const TrailingInput: React.FC<TrailingInputProps> = (props) => {
         <button
           onClick={props.onBtnClick && props.onBtnClick}
           type="button"
-          className={`-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500`}
+          className={`-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500`}
         >
           <BtnIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
           {props.btnLabel && <span>{props.btnLabel}</span>}
--- stories/TrailingInput.stories.tsx
+++ stories/TrailingInput.stories.tsx
@@ -4,13 +4,15 @@ import TrailingInput, { TrailingInputProps } from '../src/TrailingInput';
 import { EyeIcon, LockClosedIcon } from '@heroicons/react/solid';
 
 const meta: Meta = {
-    title: 'Input',
-    component: TrailingInput
+  title: 'Input',
+  component: TrailingInput,
 };
 
 export default meta;
 
-const Template: Story<TrailingInputProps> = args => <TrailingInput className='mt-1' {...args} />;
+const Template: Story<TrailingInputProps> = (args) => (
+  <TrailingInput className="mt-1" {...args} />
+);
 
 export const TrailingInputDefault = Template.bind({});
 export const TrailingInputIcon = Template.bind({});
@@ -18,31 +20,31 @@ export const TrailingInputText = Template.bind({});
 export const TrailingInputError = Template.bind({});
 
 TrailingInputDefault.args = {
-    btnIcon: EyeIcon,
-    label: 'Password',
-    input: {
-        placeholder: '••••••••',
-        id: 'login-password',
-        name: 'password',
-        type: 'password',
-        autoComplete: 'off',
-        required: true,
-    },
-    helpText: "",
-    errors: []
+  btnIcon: EyeIcon,
+  label: 'Password',
+  input: {
+    placeholder: '••••••••',
+    id: 'login-password',
+    name: 'password',
+    type: 'password',
+    autoComplete: 'off',
+    required: true,
+  },
+  helpText: '',
+  errors: null,
 };
 
 TrailingInputIcon.args = {
-    icon: LockClosedIcon,
-    ...TrailingInputDefault.args
+  icon: LockClosedIcon,
+  ...TrailingInputDefault.args,
 };
 
 TrailingInputText.args = {
-    btnLabel: 'See',
-    ...TrailingInputIcon.args
+  btnLabel: 'See',
+  ...TrailingInputIcon.args,
 };
 
 TrailingInputError.args = {
-    ...TrailingInputIcon.args,
-    errors: ['Password must be at least 8 characters.']
+  ...TrailingInputIcon.args,
+  errors: ['Password must be at least 8 characters.'],
 };
