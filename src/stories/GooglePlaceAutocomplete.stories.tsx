079f6df298b35502724c5d124dc3c1a3fd01b70d
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2025-04-22 10:01:21 +0600

    fix: place autocomplete design (#120)
    * fix: place autocomplete design
    
    * wip

--- src/stories/GooglePlaceAutocomplete.stories.tsx
+++ src/stories/GooglePlaceAutocomplete.stories.tsx
@@ -13,6 +13,18 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     apiKey: 'AIzaSyDvqF2TFlH_iL33LMiDHvNpkAcpK0nW7_0',
+    label: 'Address',
+  },
+  render: (args) => {
+    return <GooglePlaceAutocomplete {...args} />;
+  },
+};
+
+export const Error: Story = {
+  args: {
+    apiKey: 'AIzaSyDvqF2TFlH_iL33LMiDHvNpkAcpK0nW7_0',
+    errors: ['Address is required'],
+    label: 'Address',
   },
   render: (args) => {
     return <GooglePlaceAutocomplete {...args} />;
