d5e8c87aae4b37d55db183d0c2d71725af530ab6
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-05-23 12:23:38 +0600

    fix: GoogleAutocomplete build issue

--- src/components/GooglePlaceAutocomplete.tsx
+++ src/components/GooglePlaceAutocomplete.tsx
@@ -114,7 +114,7 @@ const GooglePlaceAutocomplete: FC<GooglePlaceAutocompleteProps> = ({
     });
   };
 
-  const handleScriptLoad = (autoCompleteRef: RefObject<HTMLInputElement>, countryRestriction?: string) => {
+  const handleScriptLoad = (autoCompleteRef: RefObject<HTMLInputElement | null>, countryRestriction?: string) => {
     const { maps } = window.google;
     autoComplete = new maps.places.Autocomplete(autoCompleteRef.current as HTMLInputElement);
 
@@ -136,7 +136,7 @@ const GooglePlaceAutocomplete: FC<GooglePlaceAutocompleteProps> = ({
   useEffect(() => {
     const googleURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&language=en&callback=initMapPlaceholderFunc`;
 
-    loadScript(googleURL, () => handleScriptLoad(autoCompleteRef as RefObject<HTMLInputElement>, countryRestriction));
+    loadScript(googleURL, () => handleScriptLoad(autoCompleteRef, countryRestriction));
   }, [countryRestriction]);
 
   const hasErrors = Boolean(errors && errors.length > 0);
