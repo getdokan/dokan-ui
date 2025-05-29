ea965a3d4a632e473ec3ce8ed176f0d4331b7e14
Hasibur Rahman <hasib.webdev@gmail.com>
2025-05-29 17:28:34 +0600

    refactor: remove map api key (#125)

--- src/stories/GooglePlaceAutocomplete.stories.tsx
+++ src/stories/GooglePlaceAutocomplete.stories.tsx
@@ -12,7 +12,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    apiKey: 'AIzaSyDvqF2TFlH_iL33LMiDHvNpkAcpK0nW7_0',
+    apiKey: '',
     label: 'Address',
   },
   render: (args) => {
@@ -22,7 +22,7 @@ export const Default: Story = {
 
 export const Error: Story = {
   args: {
-    apiKey: 'AIzaSyDvqF2TFlH_iL33LMiDHvNpkAcpK0nW7_0',
+    apiKey: '',
     errors: ['Address is required'],
     label: 'Address',
   },
