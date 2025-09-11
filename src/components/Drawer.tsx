7f94127e8f4592d84e7a54a3254287cd3b891e27
Raihan Ali <77913319+iamraihan@users.noreply.github.com>
2025-09-11 16:46:14 +0600

    fix:drawer component updated to radix UI and remove headless UI dependency (#128)
    * refactor: replace Headless UI Dialog with Radix UI Dialog in Drawer component and enhance animations
    
    * chore: remove @radix-ui/react-dialog dependency from package.json and pnpm-lock.yaml
    
    * fix: imporve type defination
    
    * chore: remove headless ui depedency
    
    * udpated pnpm.lock file
    
    * drawer updated to vaul
    
    * modal content overflow auto by y axis
    
    * refactor code as by suggested ai

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,70 +1,56 @@
 import { classNames } from '@/utils';
-import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, ReactNode } from 'react';
-import { HiX } from 'react-icons/hi';
+import { Drawer as VaulDrawer } from 'vaul';
+import { ReactNode, ComponentProps } from 'react';
+import { CloseIcon } from '@/components/icons/CloseIcon';
 
-export type DrawerProps = {
+export type DrawerProps = Omit<
+  ComponentProps<typeof VaulDrawer.Root>,
+  'open' | 'onOpenChange' | 'children' | 'direction' | 'fadeFromIndex'
+> & {
   direction?: 'ltr' | 'rtl';
   isOpen: boolean;
-  onClose: () => void;
   children: ReactNode;
+  setIsOpen: (open: boolean) => void;
 };
 
-const Drawer = ({ direction = 'ltr', isOpen, onClose, children }: DrawerProps) => {
-  return (
-    <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-40" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="transition-opacity ease-linear duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity ease-linear duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-        </Transition.Child>
+export const Drawer = ({ direction = 'ltr', isOpen, setIsOpen, children, ...rest }: DrawerProps) => {
+  // Close button
+  const closeButton = (
+    <VaulDrawer.Close className="flex pt-5 px-2">
+      <span className="sr-only">Close sidebar</span>
+      <CloseIcon color="white" />
+    </VaulDrawer.Close>
+  );
 
-        <div className={classNames('fixed inset-0 flex z-40', direction === 'ltr' ? 'justify-end' : 'justify-start')}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom={direction === 'ltr' ? 'translate-x-full' : '-translate-x-full'}
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo={direction === 'ltr' ? 'translate-x-full' : '-translate-x-full'}
-          >
-            <Dialog.Panel className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className={`absolute pt-2 top-0 ${direction === 'ltr' ? '-left-12' : '-right-12'}`}>
-                  <button
-                    type="button"
-                    className="ms-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    onClick={onClose}
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <HiX className="h-6 w-6 text-white" aria-hidden="true" />
-                  </button>
-                </div>
-              </Transition.Child>
-              <div className="mt-5 flex-1 h-full overflow-y-auto" dir={direction}>
-                {children}
-              </div>
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
+  // Content
+  const content = <div className="flex-1 h-full overflow-y-auto bg-white">{children}</div>;
+
+  // Render content in the correct order based on direction
+  const renderContent = () => {
+    return direction === 'ltr' ? [closeButton, content] : [content, closeButton];
+  };
+
+  return (
+    <VaulDrawer.Root
+      key={direction}
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      direction={direction === 'ltr' ? 'right' : 'left'}
+      {...rest}
+    >
+      <VaulDrawer.Portal>
+        <VaulDrawer.Overlay className="fixed z-20 inset-0 bg-gray-600/75" />
+
+        <VaulDrawer.Content
+          className={classNames(
+            'fixed top-0 h-full flex z-40 max-w-sm w-full outline-none',
+            direction === 'ltr' ? 'right-0' : 'left-0'
+          )}
+        >
+          {renderContent()}
+        </VaulDrawer.Content>
+      </VaulDrawer.Portal>
+    </VaulDrawer.Root>
   );
 };
 
