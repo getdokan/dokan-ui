0142b26f62655216e27c3d642ae581e2a483b6ee
Raihan Ali <trueraihan@gmail.com>
2025-09-11 14:38:42 +0600

    drawer updated to vaul

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,51 +1,53 @@
 import { classNames } from '@/utils';
-import * as Dialog from '@radix-ui/react-dialog';
-import { ReactNode, useCallback } from 'react';
-import { HiX } from 'react-icons/hi';
+import { Drawer as VaulDrawer } from 'vaul';
+import { ReactNode } from 'react';
+import CloseIcon from './icons/CloseIcon';
 
 export type DrawerProps = {
   direction?: 'ltr' | 'rtl';
   isOpen: boolean;
-  onClose: () => void;
   children: ReactNode;
+  setIsOpen: (open: boolean) => void;
 };
 
-const Drawer = ({ direction = 'ltr', isOpen, onClose, children }: DrawerProps) => {
-  const handleOpenChange = useCallback(
-    (open: boolean) => {
-      if (!open) onClose();
-    },
-    [onClose]
+const Drawer = ({ direction = 'ltr', isOpen, setIsOpen, children, ...rest }: DrawerProps) => {
+  // Close button
+  const closeButton = (
+    <VaulDrawer.Close className="flex pt-5 px-2">
+      <span className="sr-only">Close sidebar</span>
+      <CloseIcon color="white" />
+    </VaulDrawer.Close>
   );
 
-  return (
-    <Dialog.Root open={isOpen} onOpenChange={handleOpenChange}>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-gray-600/75 transition-opacity ease-linear duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 " />
+  // Content
+  const content = <div className="flex-1 h-full overflow-y-auto bg-white">{children}</div>;
 
-        <div className={classNames('fixed inset-0 flex z-40', direction === 'ltr' ? 'justify-end' : 'justify-start')}>
-          <Dialog.Content
-            className={classNames(
-              'relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white',
-              'transform',
-              direction === 'ltr'
-                ? 'data-[state=open]:animate-slide-in-from-right data-[state=closed]:animate-slide-out-to-right'
-                : 'data-[state=open]:animate-slide-in-from-left data-[state=closed]:animate-slide-out-to-left'
-            )}
-          >
-            <div className={`absolute pt-2 top-0 ${direction === 'ltr' ? '-left-12' : '-right-12'}`}>
-              <Dialog.Close className="ms-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white transition-opacity ease-in-out duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0">
-                <span className="sr-only">Close sidebar</span>
-                <HiX className="h-6 w-6 text-white" aria-hidden="true" />
-              </Dialog.Close>
-            </div>
-            <div className="mt-5 flex-1 h-full overflow-y-auto" dir={direction}>
-              {children}
-            </div>
-          </Dialog.Content>
-        </div>
-      </Dialog.Portal>
-    </Dialog.Root>
+  // Render content in the correct order based on direction
+  const renderContent = () => {
+    return direction === 'ltr' ? [closeButton, content] : [content, closeButton];
+  };
+
+  return (
+    <VaulDrawer.Root
+      key={direction}
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      direction={direction === 'ltr' ? 'right' : 'left'}
+      {...rest}
+    >
+      <VaulDrawer.Portal>
+        <VaulDrawer.Overlay className="fixed z-20 inset-0 bg-gray-600/75" />
+
+        <VaulDrawer.Content
+          className={classNames(
+            'fixed top-0 h-full flex z-40 max-w-sm w-full outline-none',
+            direction === 'ltr' ? 'right-0' : 'left-0'
+          )}
+        >
+          {renderContent()}
+        </VaulDrawer.Content>
+      </VaulDrawer.Portal>
+    </VaulDrawer.Root>
   );
 };
 
