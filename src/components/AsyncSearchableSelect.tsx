64a3804b2b9eed3b8cf5d275ee4fce8acb91d438
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-09-16 11:02:12 +0600

    fix: update color variables in AsyncSearchableSelect and TaggableSelect components

--- src/components/AsyncSearchableSelect.tsx
+++ src/components/AsyncSearchableSelect.tsx
@@ -68,20 +68,20 @@ const AsyncSearchableSelect = <
           ...theme,
           colors: {
             ...theme.colors,
-            primary: 'var(--colors-primary-500)',
-            primary75: 'var(--colors-primary-200)',
-            primary50: 'var(--colors-primary-100)',
-            primary25: 'var(--colors-primary-50)',
-            neutral5: 'var(--colors-gray-100)',
-            neutral10: 'var(--colors-gray-100)',
-            neutral20: 'var(--colors-gray-200)',
-            neutral30: 'var(--colors-gray-300)',
-            neutral40: 'var(--colors-gray-400)',
-            neutral50: 'var(--colors-gray-500)',
-            neutral60: 'var(--colors-gray-600)',
-            neutral70: 'var(--colors-gray-700)',
-            neutral80: 'var(--colors-gray-800)',
-            neutral90: 'var(--colors-gray-900)',
+            primary: 'var(--color-primary-500)',
+            primary75: 'var(--color-primary-200)',
+            primary50: 'var(--color-primary-100)',
+            primary25: 'var(--color-primary-50)',
+            neutral5: 'var(--color-gray-100)',
+            neutral10: 'var(--color-gray-100)',
+            neutral20: 'var(--color-gray-200)',
+            neutral30: 'var(--color-gray-300)',
+            neutral40: 'var(--color-gray-400)',
+            neutral50: 'var(--color-gray-500)',
+            neutral60: 'var(--color-gray-600)',
+            neutral70: 'var(--color-gray-700)',
+            neutral80: 'var(--color-gray-800)',
+            neutral90: 'var(--color-gray-900)',
           },
         })}
         classNames={{
@@ -120,24 +120,24 @@ const AsyncSearchableSelect = <
           }),
           multiValue: (base) => ({
             ...base,
-            background: 'var(--colors-primary-50)',
+            background: 'var(--color-primary-50)',
             borderRadius: '0.25rem',
           }),
           multiValueLabel: (base) => ({
             ...base,
-            color: 'var(--colors-primary-600)',
+            color: 'var(--color-primary-600)',
           }),
           multiValueRemove: (base) => ({
             ...base,
-            color: 'var(--colors-primary-600)',
+            color: 'var(--color-primary-600)',
             ':hover': {
-              background: 'var(--colors-primary-100)',
+              background: 'var(--color-primary-100)',
             },
           }),
           loadingMessage: (base) => ({
             ...base,
             fontSize: '0.875rem',
-            color: 'var(--colors-gray-800)',
+            color: 'var(--color-gray-800)',
           }),
           menuPortal: (base) => ({
             ...base,
@@ -146,7 +146,7 @@ const AsyncSearchableSelect = <
           noOptionsMessage: (base) => ({
             ...base,
             fontSize: '0.875rem',
-            color: 'var(--colors-gray-800)',
+            color: 'var(--color-gray-800)',
           }),
         }}
         menuPortalTarget={props.menuPortalTarget}
--- src/components/TaggableSelect.tsx
+++ src/components/TaggableSelect.tsx
@@ -63,7 +63,6 @@ const TaggableSelect = <Option, IsMulti extends boolean = false, Group extends G
         </label>
       )}
       <CreatableSelect
-        menuIsOpen
         {...props}
         inputId={id}
         // menuPortalTarget={document.body}
@@ -113,18 +112,18 @@ const TaggableSelect = <Option, IsMulti extends boolean = false, Group extends G
           }),
           multiValue: (base) => ({
             ...base,
-            background: 'var(--colors-primary-50)',
+            background: 'var(--color-primary-50)',
             borderRadius: '0.25rem',
           }),
           multiValueLabel: (base) => ({
             ...base,
-            color: 'var(--colors-primary-600)',
+            color: 'var(--color-primary-600)',
           }),
           multiValueRemove: (base) => ({
             ...base,
-            color: 'var(--colors-primary-600)',
+            color: 'var(--color-primary-600)',
             ':hover': {
-              background: 'var(--colors-primary-100)',
+              background: 'var(--color-primary-100)',
             },
           }),
           menuPortal: (base) => ({
@@ -134,27 +133,27 @@ const TaggableSelect = <Option, IsMulti extends boolean = false, Group extends G
           noOptionsMessage: (base) => ({
             ...base,
             fontSize: '0.875rem',
-            color: 'var(--colors-gray-800)',
+            color: 'var(--color-gray-800)',
           }),
         }}
         theme={(theme) => ({
           ...theme,
           colors: {
             ...theme.colors,
-            primary: 'var(--colors-primary-500)',
-            primary75: 'var(--colors-primary-200)',
-            primary50: 'var(--colors-primary-100)',
-            primary25: 'var(--colors-primary-50)',
-            neutral5: 'var(--colors-gray-100)',
-            neutral10: 'var(--colors-gray-100)',
-            neutral20: 'var(--colors-gray-200)',
-            neutral30: 'var(--colors-gray-200)',
-            neutral40: 'var(--colors-gray-400)',
-            neutral50: 'var(--colors-gray-500)',
-            neutral60: 'var(--colors-gray-600)',
-            neutral70: 'var(--colors-gray-700)',
-            neutral80: 'var(--colors-gray-800)',
-            neutral90: 'var(--colors-gray-900)',
+            primary: 'var(--color-primary-500)',
+            primary75: 'var(--color-primary-200)',
+            primary50: 'var(--color-primary-100)',
+            primary25: 'var(--color-primary-50)',
+            neutral5: 'var(--color-gray-100)',
+            neutral10: 'var(--color-gray-100)',
+            neutral20: 'var(--color-gray-200)',
+            neutral30: 'var(--color-gray-200)',
+            neutral40: 'var(--color-gray-400)',
+            neutral50: 'var(--color-gray-500)',
+            neutral60: 'var(--color-gray-600)',
+            neutral70: 'var(--color-gray-700)',
+            neutral80: 'var(--color-gray-800)',
+            neutral90: 'var(--color-gray-900)',
           },
         })}
       />
